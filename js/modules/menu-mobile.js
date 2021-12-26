5a57ef8414b778717181b70d1c9bbb974808de6e
صموئيل راموس <samuelaoliveiraramos@gmail.com>
2021-12-26 01:24:55 -0300

    correcao de alguns bugs / ainda falta corrigir a alteracao de cor

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,36 +1,37 @@
-import outsideClick from './outside-click.js';
+import outsideClick from './outside-click.js'
 
 export default class MenuMobile {
   constructor(menuButton, menuList, events) {
-    this.menuButton = document.querySelector(menuButton);
-    this.menuList = document.querySelector(menuList);
+    this.menuButton = document.querySelector(menuButton)
+    this.menuList = document.querySelector(menuList)
     this.activeClass = 'active'
     this.openMenu = this.openMenu.bind(this)
 
     if (events === undefined) {
       this.events = ['click', 'touchstart']
-    } else{
+    } else {
       this.events = events
     }
   }
-  
-  openMenu() {
-    this.menuList.classList.add(this.activeClass);
-    this.menuButton.classList.add(this.activeClass);
+
+  openMenu(event) {
+    event.preventDefault()
+    this.menuList.classList.add(this.activeClass)
+    this.menuButton.classList.add(this.activeClass)
     outsideClick(this.menuList, this.events, () => {
-      this.menuList.classList.remove(this.activeClass);
-      this.menuButton.classList.remove(this.activeClass);
-    });
+      this.menuList.classList.remove(this.activeClass)
+      this.menuButton.classList.remove(this.activeClass)
+    })
   }
 
-  addMenuMobileEvents(){
-    this.events.forEach((evento) => {
-      this.menuButton.addEventListener(evento, this.openMenu);
-    });
+  addMenuMobileEvents() {
+    this.events.forEach(evento => {
+      this.menuButton.addEventListener(evento, this.openMenu)
+    })
   }
 
-  init(){
-    if(this.menuButton && this.menuList){
+  init() {
+    if (this.menuButton && this.menuList) {
       this.addMenuMobileEvents()
     }
     return this
--- js/modules/temas.js
+++ js/modules/temas.js
@@ -1,14 +1,13 @@
 export default function initTemas() {
   function handleKeyboard(event) {
-    if (event) document.body.classList.toggle('noturno');
+    if (event) document.body.classList.toggle('noturno')
   }
-  const mudarCor = document.querySelector('[href^="mudar-cor.html"]');
-  mudarCor.addEventListener('click', handleKeyboard);
+  const mudarCor = document.querySelector('[href^="mudar-cor.html"]')
+  mudarCor.addEventListener('click', handleKeyboard)
 
   function handleClickT(event) {
-    console.log(event.key);
-    if (event) document.documentElement.classList.toggle('textomaior');
+    if (event) document.documentElement.classList.toggle('textomaior')
   }
-  const aumentarFonte = document.querySelector('[href^="aumentar-fonte.html"]');
-  aumentarFonte.addEventListener('click', handleClickT);
+  const aumentarFonte = document.querySelector('[href^="aumentar-fonte.html"]')
+  aumentarFonte.addEventListener('click', handleClickT)
 }
